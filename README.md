765e4a5b124fe2b130ac818f21068aa83b578a4b
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-11-14 13:52:31 -0800

    initialize repository

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ruby on Rails Tutorial: first application
+
+This is the first application for the [*Ruby on Rails Tutorial*](http://railstutorial.org/) by [Michael Hartl](http://michaelhartl.com/).
+
+After you finish this Ruby on Rails Tutorial, find more great tutorials and challenges, check out [*Railsbrige*](http://docs.railsbridge.org/docs/)!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '857e99534c373983d8f7a99c7c0d75fc69c5c4d74be536b06d664a128b86c25f3b78281515caebf8289d6bf7f278c16fe77c719391c4042d07a3d113e4e426ba'
